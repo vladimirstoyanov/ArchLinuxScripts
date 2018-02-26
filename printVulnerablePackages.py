c9a33a95070c05e5405fb661f038004a5f37a848
vladimirstoyanov <firojjsci@gmail.com>
2018-02-26 23:09:31 +0000

    updagted printVulnerablePackage...

--- printVulnerablePackages.py
+++ printVulnerablePackages.py
@@ -4,135 +4,141 @@ import time
 from urllib.request import urlopen
 from pkg_resources import parse_version
 
-def compareVersions(version1, version2):
-    return parse_version(version1) >= parse_version(version2)
-
-def getPackageName(html, index, source_length):
-    package_name = ""
-    index = html.find("<a", index, source_length)
-    if (index == -1):
-            print ("Tried to get package name: <a> hasn't found.")
-            return package_name, index
-    index+=len("<a")
-    while (html[index]!='>' and index < len(html)):
-        index+=1
-    index+=1
-    while(html[index]!='<' and index <len(html)):
-        package_name += html[index]
-        index+=1
+class Package:
+    def __init__(self):
+        self.index = 0
+        self.html = "" #ToDo: change this name
+        self.source_length = "" #ToDo: change this name
         
-    return package_name, index
-
-def getPackageVersion(html, index, source_length):
-    package_version = ""
-    index = html.find("<td>", index, source_length)
-    if (index==-1):
-            print ("Tried to get the version: <td> hasn't found.")
-            return package_version, index
-    index+=len("<td>")
-    while (html[index]!='<' and index<len(html)):
-        package_version+=html[index]
-        index+=1
-    return package_version, index
-
-def getPackageSeverity(html, index, source_length):
-    package_severity = ""
-    index = html.find("<span", index, source_length)
-    if (index==-1):
-            print ("Tried to get severity:  <span> hasn't found.")
-            return package_severity, index
-    index+=len("<span")
-    while (html[index]!='>' and index < len(html)):
-        index+=1
-    index+=1
-    while(html[index]!='<' and index <len(html)):
-        package_severity += html[index]
-        index+=1
-    return package_severity, index
-
-def getPackageData (index, html, source_length):
-    package_name =""
-    package_version =""
-    package_severity =""
-    
-    index = html.find("<tr>", index, source_length)
-    if (index == -1):
-        return package_name, package_version, package_severity, index
-    index+=len("<tr>")
-
-    for i in range(2):
-        index = html.find("<td>", index, source_length)
-        if (index == -1):
-            print ("Tried to get package name: <td> not found.")
-            return package_name, package_version, package_severity, index
-        index+=len("<td>")
-
-    index = html.find("<td ", index, source_length)
-    if (index == -1):
-            print ("Tried to get package name: <td> not found.")
-            return package_name, package_version, package_severity, index
-    index+=len("<td ")
         
-    #get package name
-    package_name, index = getPackageName(html, index, source_length)
-    if (index == -1):
-        return package_name, package_version, package_severity, index
-    
-    #get version
-    package_version, index = getPackageVersion(html, index, source_length)
-    if (index == -1):
-        return package_name, package_version, package_severity, index
-    
-    #get severity
-    package_severity, index = getPackageSeverity(html, index, source_length)
-    if (index == -1):
-        return package_name, package_version, package_severity, index
-    
-    return package_name, package_version, package_severity, index
-    
-    
-def getVulnerablePackagesInfomation():
-    list_packages =[]
-    response = urlopen('https://security.archlinux.org/')
-    html = response.read()
-    html = html.decode("utf-8", "strict")
-    source_length = len(html)
-    
-    index = 0
-    index = html.find("<tbody>", index, source_length)
-    if (index == -1):
-        print ("Cannot find <tbody> tag")
-        sys.exit()
-    index+=len("<tbody>")
-    
-    while (index!=-1 and index<len(html)):
-        package_name, package_version, severity, index = getPackageData(index, html, source_length)
-        if (index == -1):
+    def compareVersions(self, version1, version2):
+        return parse_version(version1) >= parse_version(version2)
+
+    def getPackageName(self):
+        package_name = ""
+        self.index = self.html.find("<a", self.index, self.source_length)
+        if (self.index == -1):
+                print ("Tried to get package name: <a> hasn't found.")
+                return package_name
+        self.index+=len("<a")
+        while (self.html[self.index]!='>' and self.index < len(self.html)):
+            self.index+=1
+        self.index+=1
+        while(self.html[self.index]!='<' and self.index <len(self.html)):
+            package_name += self.html[self.index]
+            self.index+=1
+        return package_name
+
+    def getPackageVersion(self):
+        package_version = ""
+        self.index = self.html.find("<td>", self.index, self.source_length)
+        if (self.index==-1):
+                print ("Tried to get the version: <td> hasn't found.")
+                return package_version
+        self.index+=len("<td>")
+        while (self.html[self.index]!='<' and self.index<len(self.html)):
+            package_version+=self.html[self.index]
+            self.index+=1
+        return package_version
+
+    def getPackageSeverity(self):
+        package_severity = ""
+        self.index = self.html.find("<span", self.index, self.source_length)
+        if (self.index==-1):
+                print ("Tried to get severity:  <span> hasn't found.")
+                return package_severity
+        self.index+=len("<span")
+        while (self.html[self.index]!='>' and self.index < len(self.html)):
+            self.index+=1
+        self.index+=1
+        while(self.html[self.index]!='<' and self.index <len(self.html)):
+            package_severity += self.html[self.index]
+            self.index+=1
+        return package_severity
+
+    def getPackageData (self):
+        package_name =""
+        package_version =""
+        package_severity =""
+        
+        self.index = self.html.find("<tr>", self.index, self.source_length)
+        if (self.index == -1):
+            return package_name, package_version, package_severity
+        self.index+=len("<tr>")
+
+        for i in range(2):
+            self.index = self.html.find("<td>", self.index, self.source_length)
+            if (self.index == -1):
+                print ("Tried to get package name: <td> not found.")
+                return package_name, package_version, package_severity
+            self.index+=len("<td>")
+
+        self.index = self.html.find("<td ", self.index, self.source_length)
+        if (self.index == -1):
+                print ("Tried to get package name: <td> not found.")
+                return package_name, package_version, package_severity
+        self.index+=len("<td ")
+            
+        #get package name
+        package_name = self.getPackageName()
+        if (self.index == -1):
+            return package_name, package_version, package_severity
+        
+        #get version
+        package_version = self.getPackageVersion()
+        if (self.index == -1):
+            return package_name, package_version, package_severity
+        
+        #get severity
+        package_severity = self.getPackageSeverity()
+        if (self.index == -1):
+            return package_name, package_version, package_severity
+        
+        return package_name, package_version, package_severity
+        
+        
+    def getVulnerablePackagesInfomation(self):
+        list_packages =[]
+        response = urlopen('https://security.archlinux.org/')
+        self.html = response.read()
+        self.html = self.html.decode("utf-8", "strict")
+        self.source_length = len(self.html)
+        
+        self.index = 0
+        self.index = self.html.find("<tbody>", self.index, self.source_length)
+        if (self.index == -1):
+            print ("Cannot find <tbody> tag")
             return list_packages
-        list_package_data = [package_name, package_version, severity]
-        list_packages.append(list_package_data)
+        self.index+=len("<tbody>")
         
-    return list_packages
-    
-def getLocalPackagesInformation():
-    list_local_packages = []
-    os.system("pacman -Q > /tmp/local_packages.txt")
-    f = open ("/tmp/local_packages.txt", "r")
-    for str1 in f.readlines():
-        l_package = str1.split(' ')
-        if (len(l_package)!=2):
-            continue
-        l_package[1] = l_package[1].replace('\n','')
-        l_package[1] = l_package[1].replace('\r','')
+        while (self.index!=-1 and self.index<len(self.html)):
+            package_name, package_version, package_severity = self.getPackageData()
+            if (self.index == -1):
+                return list_packages
+            list_package_data = [package_name, package_version, package_severity]
+            list_packages.append(list_package_data)
+            
+        return list_packages
         
-        list_local_packages.append(l_package)
-    f.close()
-    return list_local_packages
+    def getLocalPackagesInformation(self):
+        list_local_packages = []
+        os.system("pacman -Q > /tmp/local_packages.txt")
+        f = open ("/tmp/local_packages.txt", "r")
+        for str1 in f.readlines():
+            l_package = str1.split(' ')
+            if (len(l_package)!=2):
+                continue
+            l_package[1] = l_package[1].replace('\n','')
+            l_package[1] = l_package[1].replace('\r','')
+            
+            list_local_packages.append(l_package)
+        f.close()
+        return list_local_packages
 
 
-list_packages = getVulnerablePackagesInfomation()
-
-list_local_packages = getLocalPackagesInformation()
+package = Package()
+list_packages = package.getVulnerablePackagesInfomation()
+list_local_packages = package.getLocalPackagesInformation()
 
 f = open('/tmp/vulnerablePackages.txt', 'w')
 print_string = 'vulnerable packages:' 
@@ -142,7 +148,7 @@ print (print_string)
 for i in range (len(list_packages)):
     for j in range(len(list_local_packages)):
         if (list_packages[i][0] == list_local_packages[j][0]):
-            if (compareVersions(list_packages[i][1],list_local_packages[j][1])):
+            if (package.compareVersions(list_packages[i][1],list_local_packages[j][1])):
                 print_string = str(list_packages[i][0]) + ' ' + str(list_packages[i][1]) + ', current version: ' + str(list_local_packages[j][1])
                 f.write(print_string + '\n')
                 print (print_string)
