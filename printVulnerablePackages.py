b90c642784b02ed50e016400a8ce0fd8e766a404
vladimirstoyanov <firojjsci@gmail.com>
2018-04-15 23:42:37 +0000

    updated

--- printVulnerablePackages.py
+++ printVulnerablePackages.py
@@ -13,7 +13,7 @@ for i in range (len(list_vulnerable_packages)):
     for j in range(len(list_local_packages)):
         if (list_vulnerable_packages[i][0] == list_local_packages[j][0]):
             if (package_.compareVersions(list_vulnerable_packages[i][1],list_local_packages[j][1])):
-                print_string = str(list_vulnerable_packages[i][0]) + ' ' + str(list_vulnerable_packages[i][1]) + ', current version: ' + str(list_local_packages[j][1])
+                print_string = str(list_vulnerable_packages[i][0]) + ' ' + str(list_vulnerable_packages[i][1]) + ', current version: ' + str(list_local_packages[j][1]) + ' severity: ' + str(list_vulnerable_packages[i][2])
                 f.write(print_string + '\n')
                 print (print_string)
 f.close()
