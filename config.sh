af71224fcd1a95080c2c0eb5bace178ec4c991eb
vladimirstoyanov <firojjsci@gmail.com>
2018-05-02 09:57:05 +0000

    Creating back up of some files...

--- config.sh
+++ config.sh
@@ -11,6 +11,8 @@ sudo systemctl start wpa_supplicant.service
 
 #ToDo: replace the username with some variable
 echo "Changing default plasma theme, wallpaper, etc."
+cp /home/scitickart/.config/plasma-org.kde.plasma.desktop-appletsrc /home/scitickart/.config/plasma-org.kde.plasma.desktop-appletsrc_back
+cp /home/scitickart/.config/plasmarc /home/scitickart/.config/plasmarc_back
 cp Resources/plasma-org.kde.plasma.desktop-appletsrc /home/scitickart/.config/
 cp Resources/plasmarc  /home/scitickart/.config/
 
@@ -22,6 +24,8 @@ echo "Changing default start menu icon..."
 #cp Resources/logo250x250.svg /usr/share/icons/breeze/preferences/32/plasma.svg
 
 echo "Changing the keyboard layout and global shortcuts..."
+cp /home/scitickart/.config/kglobalshortcutsrc /home/scitickart/.config/kglobalshortcutsrc_back
+cp /home/scitickart/.config/kxkbrc /home/scitickart/.config/kxkbrc_back
 cp Resources/kglobalshortcutsrc /home/scitickart/.config/
 cp Resources/kxkbrc /home/scitickart/.config/
 
