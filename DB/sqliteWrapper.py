4da50c48c71d0a124c7db3924560bbf96ec34c55
vladimirstoyanov <firojjsci@gmail.com>
2018-08-10 03:38:20 +0000

    adding methods that create, insert, read data from sqlite db

--- DB/sqliteWrapper.py
+++ DB/sqliteWrapper.py
@@ -1,20 +1,57 @@
 import sqlite3
 
 class SQLiteWrapper:
-	def __init__(db_name):
+	def __init__(self, db_name):
 		self.db_name = db_name
-		connect(db_name)
+		self.connect(db_name)
 
-	def connect (db_name):
-		self.connect = sqlite3.connect(db_name) 
+	def connect (self, db_name):
+		self.connect = sqlite3.connect(db_name)
 
-	def getTables():
+	def createTable (self, table_name, data):
+		c = self.connect.cursor()
+		query_string = "CREATE TABLE "
+		query_string += table_name
+		query_string += " ("
+		for i in range (len(data)):
+			query_string += data[i]
+			if (i<(len(data)-1)):
+				query_string += ','
+		query_string += ')'
+		#print query_string
+		c.execute(query_string)
+
+	def insertData (self, table_name, data):
+		c = self.connect.cursor()
+		query_string = "INSERT INTO "
+		query_string += table_name
+		query_string += " VALUES ("
+		if (len(data)<0):
+			return
+
+		length = len(data[0])
+		for i in range(length):
+			query_string += "?"
+			if (i<(length-1)):
+				query_string += ","
+		query_string+=")"
+		#print query_string
+		c.executemany(query_string, data)
+
+
+	def getTables(self):
 		pass
-	
-	#tcp_data.ip.data.add(data_) where tcp_data is a table, IP is a key, data is a column, add makes adding a row with key ip and data row - data_ 	
-	def read (query):
-		pass
 
-	def write(query):
-		psas	
-	
+	def readData (self, table_name):
+		  c = self.connect.cursor()
+		  query_string = "SELECT * FROM "
+		  query_string += table_name
+		  c.execute(query_string)
+		  rows = c.fetchall()
+
+		  data = ""
+		  for row in rows:
+			  data += str(row)
+			  data += '\n'
+
+		  return data
---
+++ sqlite_test.py
@@ -0,0 +1,8 @@
+from DB import sqliteWrapper
+
+sqlite = sqliteWrapper.SQLiteWrapper("test")
+
+sqlite.createTable("team", ["name text", "number real"])
+sqlite.insertData("team", [("Ivan", 223), ("Peter", 111)])
+data = sqlite.readData("team")
+print data
