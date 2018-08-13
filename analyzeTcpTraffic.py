7623d026955d5dae2f11bb2a6aa6124f80cde18b
vladimirstoyanov <firojjsci@gmail.com>
2018-08-13 10:11:45 +0000

    Added ToDo comment

--- analyzeTcpTraffic.py
+++ analyzeTcpTraffic.py
@@ -104,8 +104,7 @@ def getCountryCityOrgName (ip_address):
 	return netName, city, country
 
 try:
-	#ToDo: use GGP (socket.getprotobyname('ggp')) instead socket.IPPROTO_TCP
-    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)#ToDo: use GGP (socket.getprotobyname('ggp')) instead socket.IPPROTO_TCP
 except socket.error , msg:
     print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
     sys.exit()
