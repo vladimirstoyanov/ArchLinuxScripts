de1a2078ef8a11207a17dc9c50a7b6c9611cd3db
vladimirstoyanov <firojjsci@gmail.com>
2018-08-23 08:46:21 +0000

    Check input arguments

--- Scan/Wireless/set_channel.sh
+++ Scan/Wireless/set_channel.sh
@@ -1,1 +1,9 @@
+if [ $# -ne 2 ]
+then
+  echo "Wrong input! please use the following input: "
+	echo "1 arg - wifi interface"
+  echo "2 arg - wifi channel"
+  exit 1
+fi
+
 iw dev $1 set channel $2 #$1 - wifi interface, $2 - channel
