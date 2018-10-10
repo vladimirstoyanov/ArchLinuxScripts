ded728bedb1a61ff379971b6f8428ede44e92947
vladimirstoyanov <firojjsci@gmail.com>
2018-10-10 10:10:07 +0000

    print vulnerable packages after update and restart the system

--- Package/update.sh
+++ Package/update.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
+
+#update the system
 pacman -Suy
+
+#print vulnerable packages
+#echo "Vulnerable packages: "
+python ../printVulnerablePackages.py
+sleep 10
+
+echo "The system will restart..."
+sleep 1
+shutdown -r now
