606e608aa8989ff282485cb436df76bdcc3c0b13
Vladimir Stoyanov <firojjsci@gmail.com>
2018-11-27 15:32:42 +0200

    Update open_core_dump.sh

--- GDB/open_core_dump.sh
+++ GDB/open_core_dump.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 #$1 - path to binary
 #$2 - path to coredump
+
+if [ $# -ne 2 ]
+then
+  echo "Wrong input! please use the following input: "
+        echo "1 arg - path to binary"
+        echo "2 arg - path to coredump"
+  exit 1
+fi
+
 gdb $1 $2
