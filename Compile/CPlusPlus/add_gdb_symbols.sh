86d18e58bddfd16fea1daf14dd723d9ae16993b3
Vladimir Stoyanov <firojjsci@gmail.com>
2018-11-27 15:29:57 +0200

    Update add_gdb_symbols.sh

--- Compile/CPlusPlus/add_gdb_symbols.sh
+++ Compile/CPlusPlus/add_gdb_symbols.sh
@@ -5,7 +5,7 @@
 if [ $# -ne 2 ]
 then
   echo "Wrong input! please use the following input: "
-        echo "1 arg  - Binary name"
+        echo "1 arg - Binary name"
         echo "2 arg - Source file name"
   exit 1
 fi
