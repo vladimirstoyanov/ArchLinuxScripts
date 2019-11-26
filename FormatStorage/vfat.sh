7600500079c413857a158b2b72715dfc9d3d039a
vladimirstoyanov <firojjsci@gmail.com>
2019-11-26 12:22:38 +0200

    added

---
+++ FormatStorage/vfat.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#$1 - A partition (for example: /dev/sda)
+if [ $# -ne 2 ]
+then
+  echo "Wrong input! please use the following input: "
+        echo "1 arg - A partition (for example: /dev/sda)"
+  exit 1
+fi
+
+mkfs.vfat $1
