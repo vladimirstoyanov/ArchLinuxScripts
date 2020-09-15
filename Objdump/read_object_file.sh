605e7dbca6f5119f4013afa73dba68dc5201cd55
vladimirstoyanov <firojjsci@gmail.com>
2020-09-15 22:46:51 +0300

    Updated

---
+++ Objdump/read_object_file.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#$1 - object file name
+
+if [ $# -ne 1 ]
+then
+  echo "Wrong input! please use the following input: "
+        echo "1 arg - Object file name"
+  exit 1
+fi
+
+objdump -d $1
