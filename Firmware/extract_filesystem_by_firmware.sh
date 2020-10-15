34348dc9ee1751ac3a3406ecb928882a36c1688e
vladimirstoyanov <firojjsci@gmail.com>
2020-10-15 13:47:10 +0300

    Added

---
+++ Firmware/extract_filesystem_by_firmware.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#$1 - filename
+if [ $# -ne 1 ]
+then
+  echo "Wrong input! please use the following input: "
+        echo "1 arg - firmware (.bin file)"
+  exit 1
+fi
+
+binwalk -e $1
