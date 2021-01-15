02b5b37cd2d675eabbd835827fd76179aa734c32
vladimirstoyanov <firojjsci@gmail.com>
2021-01-15 23:42:07 +0000

    Updated

---
+++ Users/add_sudo_user.sh
@@ -0,0 +1,8 @@
+#$1 - username
+useradd -m /home/$1
+passwd $1
+
+pacman -S sudo
+
+echo 'Add the following row in /etc/sudoers'
+echo '$1 ALL=(ALL) ALL'
--- Users/add_user.sh
+++ Users/add_user.sh
@@ -1,8 +1,3 @@
-#$1 - name of the user
-useradd $1
+#$1 - username
+useradd -m $1
 passwd $1
-
-pacman -S sudo
-
-echo 'Add the following row in /etc/sudoers'
-echo '$1 ALL=(ALL) ALL'
