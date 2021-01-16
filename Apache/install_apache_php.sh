19db99f783e981c07cda84bc09f88e018890ac61
vladimirstoyanov <firojjsci@gmail.com>
2021-01-16 23:11:12 +0000

    Updated

--- Apache/install_apache_php.sh
+++ Apache/install_apache_php.sh
@@ -6,4 +6,8 @@ echo "Installing php..."
 pacman -S php php-apache
 sleep 1
 
+cp httpd.conf /etc/httpd/conf/
+
+systemctl restart httpd.service
+
 echo "Information: /var/www/ is replaced by /srv/httpd/"
