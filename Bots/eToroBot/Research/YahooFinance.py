44f63aaadec27fcaf41881bcb9b97b4ac6948320
vladimirstoyanov <firojjsci@gmail.com>
2021-04-26 11:41:50 +0300

    Scrapping stocks description data

--- Bots/eToroBot/Research/YahooFinance.py
+++ Bots/eToroBot/Research/YahooFinance.py
@@ -18,7 +18,7 @@ from driver import Driver
 from sqliteData import SqliteDataEtoro
 from seleniumWrapper import SeleniumWrapper
 
-class YahooFinance:
+class YahooFinanceWebScraper:
     def __init__ (self):
         atexit.register(self.handleExit)
         self.stocksDataBase = SqliteDataEtoro ('stocks.db')
@@ -29,7 +29,7 @@ class YahooFinance:
         self.driver = driverObj.getDriver()
         self.seleniumWrapper  = SeleniumWrapper(self.driver)
         self.updatedStockStats = 0
-        self.scrappingStockData ()
+        self.scrappingDescriptionData ()
 
     def handleExit (self):
             self.seleniumWrapper.close()
@@ -38,10 +38,11 @@ class YahooFinance:
             print ("==============================================")
             print ("Updated stocks: " + str(self.updatedStockStats))
             print ("Trying to download stats for stock id: " + stockId)
-            url = "https://finance.yahoo.com/quote/" + stockId + "?p=" + stockId
-            self.seleniumWrapper.getRequest  (url)
+
             stockData = ""
             try:
+                url = "https://finance.yahoo.com/quote/" + stockId + "?p=" + stockId
+                self.seleniumWrapper.getRequest  (url)
                 stockData = self.seleniumWrapper.getTextByCSSSelector('#quote-summary')
             except:
                 pass
@@ -54,28 +55,9 @@ class YahooFinance:
     def scrappingStockData (self):
         for i in range (len (self.stocksData)):
             stockData = self.downloadStockData (self.stocksData[i][self.stockIdIndex])
-            self.recordData(self.stocksData[i][self.stockIdIndex], stockData)
+            self.recordStatsData(self.stocksData[i][self.stockIdIndex], stockData)
 
-
-    """
-    Previous Close 7.25
-    Open 7.26
-    Bid 7.46 x 4000
-    Ask 7.51 x 3100
-    Day's Range 7.14 - 7.56
-    52 Week Range 0.70 - 17.89
-    Volume 12,313,947
-    Avg. Volume 26,309,120
-    Market Cap 1.839B
-    Beta (5Y Monthly) 2.45
-    PE Ratio (TTM) N/A
-    EPS (TTM) -1.13
-    Earnings Date Mar 11, 2021
-    Forward Dividend & Yield N/A (N/A)
-    Ex-Dividend Date N/A
-    1y Target Est 10.00
-    """
-    def recordData (self, stockId, parsedData):
+    def recordStatsData (self, stockId, parsedData):
         dict = {
                 'Previous Close': '',
                 'Open': '',
@@ -120,8 +102,29 @@ class YahooFinance:
                                   dict['Forward Dividend & Yield'],
                                   dict['Ex-Dividend Date'])
 
+    def downloadDescriptionData (self, stockId):
+        print ("Trying to download a description for stock id: " + stockId)
+        descriptionData = ""
+        try:
+            url = "https://finance.yahoo.com/quote/" + stockId + "/profile?p=" + stockId
+            self.seleniumWrapper.getRequest  (url)
+            descriptionData = self.seleniumWrapper.getTextByCSSSelector('p.Mt\(15px\)')
+        except:
+            pass
 
-yahooFinance = YahooFinance ()
+        return descriptionData
+
+    def scrappingDescriptionData (self):
+        for i in range (len (self.stocksData)):
+            descriptionData = self.downloadDescriptionData (self.stocksData[i][self.stockIdIndex])
+            self.recordDescriptionData(self.stocksData[i][self.stockIdIndex], descriptionData)
+
+    def recordDescriptionData (self, stockId, description):
+        print (description)
+        self.stocksDataBase.insertDataIntoStockDescription (stockId, "", description)
+
+
+yahooFinance = YahooFinanceWebScraper ()
 """
 testString =
 Previous Close 7.25
