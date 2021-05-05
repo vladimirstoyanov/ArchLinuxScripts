073be3e24b8d146bea7615d4df69f20fe0a57fd5
vladimirstoyanov <firojjsci@gmail.com>
2021-05-05 11:29:39 +0300

    Added attachment method

--- Email/send_email.py
+++ Email/send_email.py
@@ -3,6 +3,8 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.header import Header
+from email.mime.image import MIMEImage
+
 
 class SendEmail:
     def __init__ (self, port, smtpServer, senderEmail, receiverEmail, password, name):
@@ -28,5 +30,41 @@ class SendEmail:
         print("Trying to login with id: " + self.senderEmail)
         server.login(self.senderEmail, self.password)
         server.sendmail(self.senderEmail, self.receiverEmail, message.as_string())
+        print("Email has been sent.")
+        server.quit()
+
+    def sendEmailWithAttachment (self, subject, bodyText, attachment):
+        strFrom = self.senderEmail
+        strTo = self.receiverEmail
+
+        msgRoot = MIMEMultipart('related')
+        msgRoot['Subject'] = subject
+        msgRoot['From'] = str(Header(self.name + '<' + self.senderEmail + '>'))
+        msgRoot['To'] = strTo
+
+        msgRoot.preamble = '====================================================='
+
+        msgAlternative = MIMEMultipart('alternative')
+        msgRoot.attach(msgAlternative)
+
+        msgText = MIMEText('This object contains the plain text content of this email.')
+        msgAlternative.attach(msgText)
+
+        msgText = MIMEText(bodyText, 'html')
+        msgAlternative.attach(msgText)
+
+        fp = open(attachment, 'rb')
+        msgImage = MIMEImage(fp.read())
+        fp.close()
+
+        msgImage.add_header('Content-ID', '<image1>')
+        msgRoot.attach(msgImage)
+
+        context = ssl.create_default_context()
+        print("Trying to connect to server: " + self.smtpServer)
+        server = smtplib.SMTP_SSL(self.smtpServer, self.port, context )
+        print("Trying to login with id: " + self.senderEmail)
+        server.login(self.senderEmail, self.password)
+        server.sendmail(self.senderEmail, self.receiverEmail, msgRoot.as_string())
         print("Email has been sent.")
         server.quit()
