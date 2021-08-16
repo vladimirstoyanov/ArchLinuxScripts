28d62e3b35435633b2b9f8895671653bf73e691f
vladimirstoyanov <firojjsci@gmail.com>
2021-08-16 15:35:25 +0300

    Create a new brach and switch to it after remote configuration for fork

--- Git/configure_remote_for_fork.sh
+++ Git/configure_remote_for_fork.sh
@@ -5,8 +5,10 @@ then
   echo "Wrong input! please use the following input: "
         echo "1 arg - Fork path"
         echo "2 arg - remote repository (e.g. https://github.com/ORIGINAL_OWNER/ORIGINAL_REPOSITORY.git)"
+        echo "3 arg - branch name (e.g. develop)"
   exit 1
 fi
 
 cd $1
 git remote add upstream $2
+git checkout -b $3
