6684913673a88207ae841ebff16a95cee8286849
vladimirstoyanov <firojjsci@gmail.com>
2021-08-16 19:03:54 +0300

    Added an output filename as argument

--- File/md5SumsOfAllFilesInDirectory.sh
+++ File/md5SumsOfAllFilesInDirectory.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
   echo "Wrong input! please use the following input: "
         echo "arg1 - path to directory"
+	echo "arg2 - output filename"
   exit 1
 fi
 
 cd $1
-find -type f -exec md5sum '{}' \; > md5sum.txt
+find -type f -exec md5sum '{}' \; > $2
