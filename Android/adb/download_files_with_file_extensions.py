f571833279fee0ffb93457e22071451697003af4
vladimirstoyanov <firojjsci@gmail.com>
2021-09-08 13:02:35 +0300

    Script that download all files with an extension using adb

---
+++ Android/adb/download_files_with_file_extensions.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+class CommandLineInput:
+    def __init__ (self):
+        self.checkInput()
+
+    def checkInput (self):
+        if (len (sys.argv)!=3):
+            print ("Wrong input:")
+            print ("1 arg: file extension")
+            print ("2 arg: destination path")
+            sys.exit()
+
+class DownloadFiles:
+    def __init__ (self):
+        commandLineInput = CommandLineInput()
+        self.createListOfFiles()
+        self.downloadFiles()
+
+    def createListOfFiles (self):
+        os.system ("sudo adb shell find ./storage/self/primary/ -iname '*." + sys.argv[1] +"' > files.txt")
+
+
+    def downloadFiles (self):
+        f = open("files.txt", 'r')
+
+        for line in f.readlines():
+            line = line.replace('\n', ' ')
+            line = line.replace('\r', ' ')
+            os.system ("adb pull " + line + " " + sys.argv[2])
+            os.system ("adb shell rm " + line) #remove all pictures
+            os.system ("adb shell sync")
+
+        f.close()
+
+        os.system ('rm files.txt')
+
+if __name__ == "__main__":
+    downloadFiles = DownloadFiles ()
