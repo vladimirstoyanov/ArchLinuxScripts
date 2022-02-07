7f565ebe3c584344a6ee6a942c72902e05ac98dc
vladimirstoyanov <firojjsci@gmail.com>
2022-02-07 14:50:12 +0200

    Simulate keyboard buttons

---
+++ Keyboard/simulate_input.py
@@ -0,0 +1,13 @@
+from pynput.keyboard import Key, Controller
+import time
+keyboard = Controller()
+keys = ['a', 'w', 's', 'd']
+i = 0
+while True:
+    keyboard.press(keys[i])
+    time.sleep(1)
+    keyboard.release(keys[i])
+    time.sleep(5)
+    i+=1
+    if (i>=len(keys)):
+        i=0
