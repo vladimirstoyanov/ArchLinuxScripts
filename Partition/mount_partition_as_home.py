3ecd36b258e077002f8e134e9f149e1707224bc1
vladimirstoyanov <firojjsci@gmail.com>
2022-05-22 12:53:24 +0000

    Added a script that mount a partition on boot time

---
+++ Partition/mount_partition_as_home.py
@@ -0,0 +1,48 @@
+import os
+import sys
+
+
+class MountPartitionFstab:
+    def __init__ (self, partition, mountDirectory):
+        self.partition = partition
+        self.mountDirectory = mountDirectory
+        self.mount ()
+
+    def __getUuid (self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+
+        os.system("blkid > " + dir_path + "/list.txt")
+        f = open (dir_path + "/list.txt", "r")
+
+        for line in f.readlines():
+            if (-1 != line.find(self.partition)):
+                l_split = line.split(' ')
+                l_split_id = l_split[1].split('=')
+                uuid = l_split_id[1]
+                uuid = uuid.replace("\"", '')
+                f.close()
+                os.system('rm ' + dir_path + '/list.txt')
+                return uuid
+
+        f.close()
+
+        os.system('rm ' + dir_path + '/list.txt')
+        return ""
+
+    def mount (self):
+        uuid = self.__getUuid ()
+        if ("" == uuid):
+            print ("Can't get UUID of partition " + self.partition)
+            sys.exit()
+        print ("UUID: " + uuid)
+
+        fstabString = "UUID=" + uuid + '\t' + self.mountDirectory + "\t\text4\t\trw,realtime\t0 0"
+
+        print ("Adding " + fstabString)
+        os.system ("echo \"" + fstabString + "\" >> /etc/fstab")
+
+if __name__ == '__main__':
+    partition = input("Partition (e.g. /dev/sda1): ")
+    mountDir = input ("Mount directory (e.g. /home/user): ")
+
+    mountPartitionFstab = MountPartitionFstab (partition, mountDir)
