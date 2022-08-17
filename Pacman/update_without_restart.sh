98c1020e0b7013c5be4bafbea770f1dc9ce27b18
vladimirstoyanov <firojjsci@gmail.com>
2022-08-17 23:30:52 +0300

    Update: automatic agreement

--- Pacman/update_without_restart.sh
+++ Pacman/update_without_restart.sh
@@ -4,4 +4,4 @@ UPDATE_FILE=/tmp/updateLog.txt
 
 #update the system
 echo "==============Trying to update the system..." >> $UPDATE_FILE 
-pacman -Suy >> $UPDATE_FILE
+echo 'y' | pacman -Suy >> $UPDATE_FILE
