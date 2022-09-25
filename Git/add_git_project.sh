0bdc3a2544565cc75e52a15fa4fd7103d4588aa4
vladimirstoyanov <firojjsci@gmail.com>
2022-09-25 15:04:08 +0300

    Updated

--- Git/add_git_project.sh
+++ Git/add_git_project.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 #$1 - full path to the project
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
   echo "Wrong input! please use the following input: "
         echo "1 arg - Full path to the project"
+	echo "2 arg - Username"
   exit 1
 fi
 
 mkdir -p $1
+chown $2 $1
 cd $1
 git init --bare
+chmod -R 777 *
