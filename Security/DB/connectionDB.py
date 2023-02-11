52e575217330fba6470b99eefbfc37bd04837165
vladimirstoyanov <firojjsci@gmail.com>
2023-02-11 08:29:12 +0200

    AnalyzeTcpTraffic: added 'description' column in db

--- Security/DB/connectionDB.py
+++ Security/DB/connectionDB.py
@@ -9,11 +9,11 @@ class ConnectionDB:
         if (self.__sqlite.isTableExist("ip_info") == False):
             self.__sqlite.createTable("ip_info", ["ip_address text", "net_name text", "city text", "country text"])
         if (self.__sqlite.isTableExist("network_package_data") == False):
-            self.__sqlite.createTable("network_package_data", ["time text", "ip_address text", "port text", "process text", "data text"])
+            self.__sqlite.createTable("network_package_data", ["time text", "ip_address text", "port text", "process text", "data text", "description text"])
 
 
     def insertIpInfo (self, ip_address, net_name, city, country):
         self.__sqlite.insertData("ip_info", [(str(ip_address), str(net_name), str(city), str(country))])
 
     def insertNetworkPackageData(self, time, ip_address, port, process, data):
-        self.__sqlite.insertData("network_package_data", [(str(time), str(ip_address), str(port), str(process), data)])
+        self.__sqlite.insertData("network_package_data", [(str(time), str(ip_address), str(port), str(process), data, "")])
