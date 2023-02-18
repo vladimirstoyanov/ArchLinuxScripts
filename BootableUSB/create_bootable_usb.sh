562138d7b070f46ae16d6dbcce61fbdeefc6a464
vladimirstoyanov <firojjsci@gmail.com>
2023-02-18 18:21:36 +0200

    added --force flag

--- BootableUSB/create_bootable_usb.sh
+++ BootableUSB/create_bootable_usb.sh
@@ -3,10 +3,10 @@
 if [ $# -ne 2 ]
 then
   echo "Wrong input! please use the following input: "
-        echo "1 arg - path to flash usb drive (/dev/sbx)"
+        echo "1 arg - path to flash usb drive (e.g. /dev/sde, not /dev/sde1)"
         echo "2 arg - path to .iso file"
   exit 1
 fi
 
-wipefs --all $1
+wipefs --all --force $1
 dd bs=4M if=$2 of=$1 status=progress oflag=sync
