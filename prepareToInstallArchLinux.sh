d1c87e08c6b63ce352cc8d79d82981f9a1345b77
vladimirstoyanov <firojjsci@gmail.com>
2023-02-18 16:17:16 +0200

    Updated prepareToInstallArchLinux.py

--- prepareToInstallArchLinux.sh
+++ prepareToInstallArchLinux.sh
@@ -3,19 +3,25 @@
 if [ $# -ne 2 ]
 then
   echo "Wrong input! please use the following input: "
-        echo "1 arg - root partition (e.g. /dev/sda1)"
-        echo "2 arg - swap partition (e.g. /de/sda2)"
+        echo "1 arg - root partition (e.g. sda1)"
+        echo "2 arg - home partition (e.g. sda2)"
+        echo "3 arg - swap partition (e.g. sda3)"
         echo "Make sure that you have configured the parition table (e.g. with cfisk command)"
   exit 1
 fi
 
-gpg --keyserver-options auto-key-retrieve --verify archlinux-version-x86_64.iso.sig
-pacman-key -v archlinux-version-x86_64.iso.sig
+echo "If you don't know the iso version, then cat version file in the root directory."
+
+gpg --keyserver-options auto-key-retrieve --verify archlinux-2022.05.01-x86_64.iso.sig
+pacman-key -v archlinux-version-2022.05.01-x86_64.iso.sig
 timedatectl set-ntp true
 mkfs.ext4 /dev/$1
-mkswap /dev/$2
+mkswap /dev/$3
 mount /dev/$1 /mnt
-swapon /dev/$2
+swapon /dev/$3
 pacstrap /mnt base linux linux-firmware
+mkdir /mnt/home
+mkfs.ext4 /dev/$2
+mount /dev/$2 /mnt/home
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
