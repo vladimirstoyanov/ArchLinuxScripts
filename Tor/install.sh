1d5a780273f43d4109e9e0003d22104b9324092a
vladimirstoyanov <firojjsci@gmail.com>
2023-02-20 19:41:00 +0000

    Tor install script: added noconfirm option

--- Tor/install.sh
+++ Tor/install.sh
@@ -1,6 +1,6 @@
-sudo pacman -S tor
-sudo pacman -S torbrowser-launcher
-sudo pacman -S nyx
+sudo pacman --noconfirm -S  tor
+sudo pacman --noconfirm -S torbrowser-launcher
+sudo pacman --noconfirm -S  nyx
 
 sudo systemctl enable tor.service
 sudo systemctl start tor.service
