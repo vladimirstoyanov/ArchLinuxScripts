f8db95c05b0d91f493a1c95aca9ba7a51cbffb6b
vladimirstoyanov <firojjsci@gmail.com>
2023-02-20 19:34:41 +0000

    Auto update install script: add sudo prefix to some commands

--- Daemons/auto_update/install.sh
+++ Daemons/auto_update/install.sh
@@ -3,11 +3,11 @@ echo "===Compiling auto update daemon..."
 gcc -o auto_update auto_update.c
 
 echo "===Coping auto_update binary and update_without_restart.sh script" 
-cp auto_update /usr/bin/
-cp ../../Pacman/update_without_restart.sh /usr/bin/
+sudo cp auto_update /usr/bin/
+sudo cp ../../Pacman/update_without_restart.sh /usr/bin/
 
 echo "Generating a systemd service file..." 
 cd ../../Systemd/
-python start_daemon_at_boot_time_with_root_privileges.py AutoUpdate /usr/bin/auto_update AutoUpdate
+sudo python start_daemon_at_boot_time_with_root_privileges.py AutoUpdate /usr/bin/auto_update AutoUpdate
  
 
