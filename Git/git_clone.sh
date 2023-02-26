20ff98b04cba4e5e8b0c32ccc5baded44fc1683f
vladimirstoyanov <firojjsci@gmail.com>
2023-02-26 12:10:14 +0200

    updated git clone script

--- Git/git_clone.sh
+++ Git/git_clone.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-if [ $# -ne 3 ]
+if [ $# -ne 4 ]
 then
   echo "Wrong input! please use the following input: "
 	echo "1 arg - remote user name"
 	echo "2 arg - remote ip"
 	echo "3 arg - remote path of the project"
+  echo "4 arg - local directory"
   exit 1
 fi
 
+cd $4
 git clone $1@$2:$3
