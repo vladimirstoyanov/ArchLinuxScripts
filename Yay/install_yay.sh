08dde329b4b67791f742b146192016784cd6794a
vladimirstoyanov <firojjsci@gmail.com>
2023-05-15 21:23:24 +0300

    install yay script: added input arguments for a group and a user

--- Yay/install_yay.sh
+++ Yay/install_yay.sh
@@ -1,5 +1,14 @@
+#!/bin/sh
+if [ $# -ne 2 ]
+then
+  echo "Wrong input! please use the following input: "
+	echo "1 arg - user"
+  echo "2 arg - group"
+  exit 1
+fi
+
 cd /opt
 sudo git clone https://aur.archlinux.org/yay-git.git
-sudo chown -R vladimir:vladimir ./yay-git
+sudo chown -R $1:$2 ./yay-git
 cd yay-git
 makepkg -si
