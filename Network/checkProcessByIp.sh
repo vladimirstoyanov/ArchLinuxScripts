01de41a2456086ccc96b530e6cf1bf737ff42929
vladimirstoyanov <firojjsci@gmail.com>
2023-06-26 08:04:38 +0300

    Check which process connects to a specific IP address

---
+++ Network/checkProcessByIp.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if [ $# -ne 1 ]
+then
+  echo "Wrong input! please use the following input: "
+        echo "1 arg - IP address"
+  exit 1
+fi
+
+while true; do
+    netstat -tulnep | grep $1
+    sleep 1
+done
