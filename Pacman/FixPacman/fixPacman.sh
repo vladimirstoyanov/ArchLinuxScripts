4b0abfa6d8615485b16f85015547d61d16a2455f
vladimirstoyanov <firojjsci@gmail.com>
2023-11-26 10:32:46 +0200

    noconfirm flags for fix pacman scripts

--- Pacman/FixPacman/fixPacman.sh
+++ Pacman/FixPacman/fixPacman.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 sudo rm /var/lib/pacman/db.lck
 sudo rm -r /etc/pacman.d/gnupg
-sudo pacman -Sy gnupg archlinux-keyring
+sudo pacman -Sy --noconfirm gnupg archlinux-keyring
 sudo pacman-key --init
 sudo pacman-key --populate archlinux
 sudo pacman-key --refresh-keys
-sudo pacman -Sc
+sudo pacman -Sc --noconfirm
 
 rm -R /var/lib/pacman/sync
-sudo pacman -Syy archlinux-keyring
-sudo pacman -Syyu
+sudo pacman -Syy --noconfirm archlinux-keyring
+sudo pacman -Syyu --noconfirm
