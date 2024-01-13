722cbf1d6ffcb7101b86a9abf19e67b101dfec00
vladimirstoyanov <firojjsci@gmail.com>
2024-01-13 10:43:23 +0200

    Updated start motion script

--- Video/Motion/start_motion.sh
+++ Video/Motion/start_motion.sh
@@ -1,2 +1,10 @@
+if [ $# -ne 2 ]
+then
+  echo "Wrong input! please use the following input: "
+        echo "1 arg - time before execute in seconds"
+        echo "2 arg - path to motion.conf"
+  exit 1
+fi
+
 sleep 300
 motion -n -c motion.conf
