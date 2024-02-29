65972fe3b366180906725fda1b048d46c2d8a855
vladimirstoyanov <firojjsci@gmail.com>
2024-02-29 23:58:01 +0200

    iptables: updated scripts

--- Firewall/block_ip_addresses_range.sh
+++ Firewall/block_ip_addresses_range.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
   echo "Wrong input! please use the following input: "
         echo "1 arg - network interface (e.g. wlan0)"
-        echo "1 arg - IP range (192.168.100.0/24)"
+        echo "2 arg - IP range (192.168.100.0/24)"
   exit 1
 fi
 
-sudo iptables -i $1 -A INPUT -s $1 -j DROP
-sudo iptables -i $1 -A OUTPUT -s $1 -j DROP
-sudo iptables -i $1 -A FORWARD -s $1 -j DROP
+sudo iptables -i $1 -A INPUT -s $2 -j DROP
+sudo iptables -A OUTPUT -s $2 -j DROP
+sudo iptables -i $1 -A FORWARD -s $2 -j DROP
 
 iptables-save > /etc/iptables/iptables.rules
