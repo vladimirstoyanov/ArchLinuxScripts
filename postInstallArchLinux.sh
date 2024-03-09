c6e0158b3fe2743b4b8ddf9f0d713797c49328f7
vladimirstoyanov <firojjsci@gmail.com>
2024-03-09 15:12:31 +0000

    Added installing grub and configuring grub in config.sh

--- postInstallArchLinux.sh
+++ postInstallArchLinux.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]
+if [ $# -ne 2s ]
 then
   echo "Wrong input! please use the following input: "
         echo "1 arg - username"
+        echo "2 arg - boot partition (e.g. /dev/sda)"
   exit 1
 fi
 
@@ -26,5 +27,5 @@ echo "Creating a user $1"
 sh ./Users/add_sudo_user.sh $1
 
 sh install.sh
-sh config.sh
+sh config.sh $2
 exit
