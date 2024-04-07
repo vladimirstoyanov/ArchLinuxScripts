6d0d8dc5814f48834e6478995114f73f25d997e0
vladimirstoyanov <firojjsci@gmail.com>
2024-04-07 10:08:49 +0300

    Updated audio scripts

---
+++ Hardware/Audio/list_audio_video_devices.sh
@@ -0,0 +1,1 @@
+wpctl status
--- Hardware/Audio/restart_pipewire_pulse.sh
+++ Hardware/Audio/restart_pipewire_pulse.sh
@@ -2,4 +2,3 @@ systemctl --user restart wireplumber pipewire pipewire-pulse
 
 systemctl --user status pipewire-pulse.service
 systemctl --user restart pipewire-pulse.service
-sudo systemctl --user restart pulseaudio.socket
