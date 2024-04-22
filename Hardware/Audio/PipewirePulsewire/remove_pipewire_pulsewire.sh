50fa91ab3d2142688ce92c6297fef097e7cbebac
vladimirstoyanov <firojjsci@gmail.com>
2024-04-22 21:28:21 +0300

    Ram Disk Enviroment scripts

---
+++ Hardware/Audio/PipewirePulsewire/remove_pipewire_pulsewire.sh
@@ -0,0 +1,2 @@
+rm /usr/lib/systemd/user/pipewire.socket
+rm /usr/lib/systemd/user/pipewire.service
---
+++ Kernel/InitalRamDiskEnviroment/customized_generation.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ $# -ne 1 ]
+then
+  echo "Wrong input! please use the following input: "
+        echo "1 arg - image file (e.g. initramfs-linux.img)"
+        echo "2 arg - kernel version (e.g. 5.7.12-arch1-1. You can use uname -a command.)"
+  exit 1
+fi
+
+mkinitcpio --generate $1 --kernel $2
