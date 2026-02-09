d378459e3e8031aa9fe16671fd62d3a34bfb2ebd
vladimirstoyanov <firojjsci@gmail.com>
2026-02-09 09:26:02 +0200

    Remove not used packages

--- Torrent/install_tools.sh
+++ Torrent/install_tools.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-sudo pacman -S mktorrent
 sudo pacman -S transmission-cli
--- install.sh
+++ install.sh
@@ -10,6 +10,15 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     sleep 1
 done < "Package/pacman_packages"
 
+#remove not used packages
+echo "Removing packman packages..."
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    echo "Removing: $line"
+    pacman --noconfirm -Rs $line
+    sleep 1
+done < "Package/not_used_packages"
+
+
 echo "Installing pip packages..."
 while IFS='' read -r line || [[ -n "$line" ]]; do
     echo "Installing: $line"
