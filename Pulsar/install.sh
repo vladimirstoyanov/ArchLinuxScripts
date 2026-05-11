ba6f9f84c5d35373da94997a775cc9da6a1b61a6
vladimirstoyanov <firojjsci@gmail.com>
2026-05-11 14:46:08 +0300

    Pulsar: added copy a custom config and keymapping

--- Pulsar/install.sh
+++ Pulsar/install.sh
@@ -13,3 +13,7 @@ cd $1
 wget https://github.com/pulsar-edit/pulsar/releases/pulsar/releases/download/v1.131.3/Linux.Pulsar-1.131.3.AppImage
 chmod +x Linux.Pulsar-1.131.3.AppImage
 sudo ln -sf Linux.Pulsar-1.131.3.AppImage /usr/bin/
+
+#copy some settings (disabled auto bracket module, some key mappings)
+cp config.cson $HOME/.pulsar/
+cp keymap.cson $HOME/.pulsar/
